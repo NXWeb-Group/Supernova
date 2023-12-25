1feba7d4f48459f70aafecfc417829be12b57c6e
haster <andrew@nxweb.com>
2023-12-25 12:46:05 -0500

    games

---
+++ src/components/games.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const items = ref([]);
+
+const fetchStuff = async () => {
+  const response = await fetch('/games.json')
+  const games = await response.json()
+  items.value = games
+  console.log(items)
+}
+onMounted(fetchStuff)
+
+</script>
+
+<template>
+  <a v-for="game in items" :href="game.path">{{ game.name }}</a>
+</template>
