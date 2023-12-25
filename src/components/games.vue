6c85181c864b2eb8f7ef55eaedcff8aaaa06b9e3
haster <andrew@nxweb.com>
2023-12-25 18:59:00 -0500

    games in iframe

--- src/components/games.vue
+++ src/components/games.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+const idk = ref('select')
 const items = ref([]);
+var path = ''
 
 const fetchStuff = async () => {
   const response = await fetch('/games.json')
@@ -11,8 +13,14 @@ const fetchStuff = async () => {
 }
 onMounted(fetchStuff)
 
+function select(a){
+  path = a
+  idk.value = 'game'
+  console.log(path)
+}
 </script>
 
 <template>
-  <a class="btn2" v-for="game in items" :href="game.path"><img class="btn2" width="200vh" height="200vh" :src="game.img"></a>
+<img v-if="idk.includes('select')" v-for="game in items" @click="select(game.path)" class="btn2" width="200vh" height="200vh" :src="game.img">
+<iframe v-if="idk.includes('game')" :src="path" scrolling="no" style="border: 0px; height: 100vh; width: 100%;"></iframe>
 </template>
