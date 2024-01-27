ffb47ea5c90c4744005f431726ccc84dc9949c41
hamster <andrew@nxweb.com>
2024-01-27 15:15:30 -0500

    app.json

--- src/components/apps.vue
+++ src/components/apps.vue
@@ -1,17 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const items = ref([]);
+
+const fetchStuff = async () => {
+  const response = await fetch('/apps.json')
+  const apps = await response.json()
+  items.value = apps
+}
+onMounted(fetchStuff)
+</script>
+
 <template>
     <h1 class="title2 text-center">Apps</h1>
     <div class="d-flex justify-content-center">
-      <table>
-        <tr>
-          <td><a href="/uv/service/hvtrs8%2F-dksaopd%2Ccmm-"><img class="image" src="/pics/apps/discord.webp" alt="discord"></a></td>
-          <td><a href="/uv/service/hvtrs8%2F-wuw%2Cymuvu%60e%2Ccmm-"><img class="image" src="/pics/apps/youtube.webp" alt="youtube"></a></td>
-          <td><a href="/uv/service/hvtrs8%2F-wuw%2Ctkkvoi.aoo%2Fgxrlmrg%3F"><img class="image" src="/pics/apps/tiktok.webp" alt="tiktok"></a></td>
-        </tr>
-        <tr>
-          <td><a href="/uv/service/hvtrs8%2F-tuivtgr%2Ccmm-"><img class="image" src="/pics/apps/twitter.webp" alt="twitter"></a></td>
-          <td><a href="/uv/service/hvtrs8%2F-wuw%2Ctuivcj.vv-"><img class="image" src="/pics/apps/twitch.webp" alt="twitch"></a></td>
-          <td><a href="/webretro/index.html"><img class="image" src="/pics/apps/webretro.webp" alt="WebRetro"></a></td>
-        </tr>
-      </table>
+      <div v-for="app in items" style="width: 28vh;">
+        <a :href="app.href"><img class="image" :src="app.img" :alt="app.alt"></a>
+      </div>
     </div>
 </template>
--- src/components/credits.vue
+++ src/components/credits.vue
@@ -2,4 +2,5 @@
     <h1 class="text">Credits:</h1>
     <p class="text">This software is based off of Ultraviolet by Titanium Network: <a href="https://github.com/titaniumnetwork-dev/Ultraviolet-App">link</a></p>
     <p class="text">The backgroud image is from: <a href="https://www.hippopx.com/">hippopx</a></p>
+    <p class="text">The emulator used is WebRetro from BinBashBannana located <a href="https://github.com/BinBashBanana/webretro">here</a></p>
 </template>
