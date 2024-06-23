3b02ed9d939f076a858b5e67623b4c429ed9f62f
Andrew <andrew@nxweb.com>
2024-06-23 12:40:09 -0400

    stuff

--- src/components/tos.vue
+++ src/components/tos.vue
@@ -2,9 +2,9 @@
     <h1 class="text-white text-5xl font-semibold">Terms Of Service</h1>
     <h2 class="text-white text-4xl font-semibold">By using this service you agree to the following</h2>
     <div class="m-2">
-        <p class="text-white text-xl">We do not take resposibility for anything someone does on this site.</p>
-        <p class="text-white text-xl">We only collect data such as ip address, browser information and other data found in a standered web request.</p>
+        <p class="text-white text-xl">We do not take responsibility for anything someone does on this site.</p>
+        <p class="text-white text-xl">We only collect data such as IP address, browser information, and other data found in a standard web request.</p>
         <p class="text-white text-xl">We use cookies to store data including preferences. This data is not available to the public.</p>
-        <p class="text-white text-xl">If there are any problems contact me at andrew@nxweb.xyz</p>
+        <p class="text-white text-xl">If there are any problems contact us at support@nxweb.xyz</p>
     </div>
 </template>
